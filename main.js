7162e12a65dcd8e000f578527f41792f7dc7c24c
roj42 <roger.lampe@hyperproductive.com>
2017-03-20 11:50:52 -0400

    version for db reload adjust

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //A botkit based guildwars helperbot
 //Main controls data load and coordinates the node files
 //Author: Roger Lampe roger.lampe@gmail.com
-var version = "2.20.12"; //added checks for API status
+var version = "2.20.13"; //added checks for API status
 debug = false; //for debug messages, passed to botkit
 start = 0; //holds start time for data loading
 var toggle = true; //global no-real-use toggle. Used at present to compare 'craft' command output formats.
